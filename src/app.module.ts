b0001936c52f149687d8ef4a429aaf9d0660551b
Денис Коваленко <103291152+TwinDen0@users.noreply.github.com>
2024-12-19 10:40:36 +1000

    try fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot/bot.servise';
 import { PrismaService } from './prisma.service';
-import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [UserModule],
+  controllers: [UserController],
   providers: [BotService, PrismaService],
 })
 export class AppModule {}
