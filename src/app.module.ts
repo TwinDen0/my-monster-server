17b95fb5e9a3b59dc1af42364d074a5818caaaaf
Денис Коваленко <103291152+TwinDen0@users.noreply.github.com>
2024-12-19 10:11:30 +1000

    find error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot/bot.servise';
 import { PrismaService } from './prisma.service';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  // imports: [UserModule],
   providers: [BotService, PrismaService],
 })
 export class AppModule {}
