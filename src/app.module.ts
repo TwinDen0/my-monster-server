6e000d494e712e1715138e1c31314cdda587c332
Денис Коваленко <103291152+TwinDen0@users.noreply.github.com>
2024-12-19 10:29:14 +1000

    try add user client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BotService } from './bot/bot.servise';
 import { PrismaService } from './prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  // imports: [UserModule],
+  imports: [UserModule],
   providers: [BotService, PrismaService],
 })
 export class AppModule {}
