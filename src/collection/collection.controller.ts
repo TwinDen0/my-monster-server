f3b2febd36c414bf82bb979f72a2d8a15d0fe85b
Денис Коваленко <103291152+TwinDen0@users.noreply.github.com>
2024-12-27 14:08:11 +1000

    fix last commit

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { CollectionService } from './collection.service';
 
@@ -6,9 +6,8 @@ import { CollectionService } from './collection.service';
 export class CollectionController {
   constructor(private readonly collectionService: CollectionService) {}
 
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Старт эволюции' })
   @Get('start_evo')
+  @ApiOperation({ summary: 'Старт эволюции' })
   async startEvo(@Query() query: { tgId: string; collectionId: string }) {
     const { tgId, collectionId } = query;
     return await this.collectionService.startEvo(tgId, collectionId);
